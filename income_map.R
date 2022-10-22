f2442799718e97ef81507ce577c33d3637f53ebb
TR <tvramadoss@ucdavis.edu>
2022-10-22 16:52:45 -0700

    Trying leaflet chloropleth maps in R

--- income_map.R
+++ income_map.R
@@ -1,15 +1,3 @@
----
-title: "A to ZEV"
-author: "Trisha Ramadoss"
-date: '2022-10-22'
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-```{r echo = FALSE, results = FALSE, message =-FALSE}
 library(tidyverse)
 library(leaflet)
 
@@ -17,12 +5,13 @@ income_data <- sf::st_read(dsn = "income_distribution/income_distribution.shp")
 income_map <- sf::st_transform(income_data, "WGS84")
 m <- leaflet(income_map)
 
-```
-
-```{r echo = FALSE, message = FALSE}
-
 bins <- c(-Inf, 2.018e-05, 6.431e-05, 1.715e-04, 0.001, 0.002, Inf)
 pal <- colorBin("YlOrRd", domain = income_map$pr__200, bins = bins)
+
+labels <- sprintf(
+  "<strong>%s</strong><br/>$%g <sup>2</sup>",
+  income_map$NAME, income_map$pr__200
+) %>% lapply(htmltools::HTML)
 
 m %>% addPolygons(
   fillColor = ~pal(pr__200),
@@ -36,5 +25,9 @@ m %>% addPolygons(
     color = "#666",
     dashArray = "",
     fillOpacity = 0.7,
-    bringToFront = TRUE))
-```
+    bringToFront = TRUE),
+  label = labels,
+  labelOptions = labelOptions(
+    style = list("font-weight" = "normal", padding = "3px 8px"),
+    textsize = "15px",
+    direction = "auto"))
