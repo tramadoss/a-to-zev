9a210021dc2680e4a517a2939d21e289cd03eee0
TR <tvramadoss@ucdavis.edu>
2022-10-22 19:39:28 -0700

    working ca millionaire revenue map

--- income_map.R
+++ income_map.R
@@ -2,26 +2,38 @@ library(tidyverse)
 library(leaflet)
 
 income_data <- sf::st_read(dsn = "income_distribution/income_distribution.shp")
-income_map <- sf::st_transform(income_data, "WGS84")
-m <- leaflet(income_map)
 
-bins <- c(-Inf, 2.018e-05, 6.431e-05, 1.715e-04, 0.001, 0.002, Inf)
-pal <- colorBin("YlOrRd", domain = income_map$pr__200, bins = bins)
+new_vars <- income_data %>% sf::st_drop_geometry() %>% 
+  mutate(nickname = substr(NAME, 8, (nchar(NAME) -12))) %>% 
+  mutate(taxable_millionaires = ovr_200 * 0.02472) %>% ## % of people who make over 200k who make over 2m
+  mutate(revenue = pr__200 * 2885050875) %>% 
+  mutate(revenue_label = case_when(
+    revenue < 1000 ~"< $1k",
+    revenue > 1000000 ~ paste0("$",round(revenue/1000000,1) %>% as.character(), "m"),
+    revenue < 10000 ~ paste0("$",round(revenue/1000,1) %>% as.character(), "k"),
+    TRUE ~ paste0("$",round(revenue/1000) %>% as.character(), "k")
+  ))
+  
+income_map <- sf::st_transform(income_data, "WGS84") %>% 
+  inner_join(new_vars)
+
+bins <- c(0, 1, 20, 40, 60, 120)
+pal <- colorBin("YlOrRd", domain = income_map$taxable_millionaires, bins = bins)
 
 labels <- sprintf(
-  "<strong>%s</strong><br/>$%g <sup>2</sup>",
-  income_map$NAME, income_map$pr__200
+  "<strong>%s</strong><br/>%s <br/>%g millionaires",
+  income_map$nickname, income_map$revenue_label , round(income_map$taxable_millionaires,1)
 ) %>% lapply(htmltools::HTML)
 
-m %>% addPolygons(
-  fillColor = ~pal(pr__200),
-  weight = 2,
+m <- leaflet(income_map) %>% addPolygons(
+  fillColor = ~pal(taxable_millionaires),
+  weight = 0.01,
   opacity = 1,
-  color = NA,
+  color = "NA",
   dashArray = "3",
   fillOpacity = 0.7,
   highlightOptions = highlightOptions(
-    weight = 1,
+    weight = 0.5,
     color = "#666",
     dashArray = "",
     fillOpacity = 0.7,
@@ -29,5 +41,8 @@ m %>% addPolygons(
   label = labels,
   labelOptions = labelOptions(
     style = list("font-weight" = "normal", padding = "3px 8px"),
-    textsize = "15px",
-    direction = "auto"))
+    #textsize = "12px",
+    direction = "auto")) %>% 
+  addLegend(pal = pal, values = ~taxable_millionaires, opacity = 0.7, title = NULL,
+            position = "bottomright")
+m
